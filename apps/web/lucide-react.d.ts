141231dded60b37df423adc44a53e90a75f6bc05
Usama Nasar <usamanasar38@gmail.com>
2025-07-02 21:40:27 +0500

    chore: allow only suffixed lucide icons to prevent conflict with components

---
+++ apps/web/lucide-react.d.ts
@@ -0,0 +1,4 @@
+declare module "lucide-react" {
+    // This will re-export all exports but only includes the icons with prefixes and other library types
+  export * from "lucide-react/dist/lucide-react.suffixed";
+}
